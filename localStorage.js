016ab32c6123e4073e792b783d2bf91aca4120bb
sarah-svg <kjskillen@gmail.com>
2021-04-08 15:19:35 -0700

    update

--- localStorage.js
+++ localStorage.js
@@ -10,3 +10,14 @@ export function setUser(user){
     const stringyUser = JSON.stringify(user);
     localStorage.setItem(USER, stringyUser);
 }
+
+export function updateUserGivenChoice(questId, choice){
+    const user = getUser();
+
+    user.hp += choice.hp;
+    user.gold += choice.gold;
+
+    user.completed[questId] = true;
+
+    setUser(user);
+}
