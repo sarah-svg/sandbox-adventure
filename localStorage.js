a790e3faf88f49994028ab8f1e8ce5083d31bbb2
sarah-svg <kjskillen@gmail.com>
2021-04-08 15:16:46 -0700

    setUser

--- localStorage.js
+++ localStorage.js
@@ -5,3 +5,8 @@ export function getUser(){
     if (!user) return {};
     return JSON.parse(user);
 }
+
+export function setUser(user){
+    const stringyUser = JSON.stringify(user);
+    localStorage.setItem(USER, stringyUser);
+}
